de3d51236da39e290e0ba8c030d5dce1e3972132
Supzero-0 <mat.lecanu@gmail.com>
2025-01-13 14:37:31 +0100

    Feat: Adding Layout component, and updating routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Layout</div>}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<div>Home</div>} />
           <Route path="/beers" element={<div>Beers</div>} />
           <Route path="/breweries" element={<div>Breweries</div>} />
